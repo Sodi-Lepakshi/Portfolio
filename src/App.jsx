47daa988a46f24302422d19e5821a5e866c65bcc
Sodi-Lepakshi <lepakshisodi@gmail.com>
2025-04-13 17:08:02 +0530

    second commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import Navbar from "./components/Navbar";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Skills from "./pages/Skills";
-import Projects from "./pages/projects";
+import Projects from "./pages/Projects";
 import Contact from "./pages/Contact";
 import Footer from "./components/Footer";
 import { FaArrowUp } from "react-icons/fa";
